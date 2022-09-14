f8a25efd66eac86a4c1da6d6541f2405c64734a4
Nelson Buainain <nnbuainain@gmail.com>
2022-09-14 08:40:48 -0400

    Instantiate sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+    
 app = Flask(__name__)
 
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
+
 if __name__ == '__main__':
+    from sqlalchemy import banco
+    banco.init_app(app)
     app.run(debug = True)
